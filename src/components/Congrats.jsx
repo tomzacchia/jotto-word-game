8b3f78282633609dfcda683a46f6b19313319f29
tomzacchia <tomzacchia@live.com>
2020-02-25 12:30:26 -0500

    Add test for Congrats

---
+++ src/components/Congrats.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// receive success value -> show congrats if true
+function Congrats(props) {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default Congrats;
