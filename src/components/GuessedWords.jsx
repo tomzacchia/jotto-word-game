65123db09281878ead7bc0d505bb2bc0bfc1a159
tomzacchia <tomzacchia@live.com>
2020-02-28 15:41:35 -0500

    Add instructions test for GuessedWords

--- src/components/GuessedWords.jsx
+++ src/components/GuessedWords.jsx
@@ -2,9 +2,22 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const GuessedWords = props => {
-  return <div></div>;
+  let content;
+  if (props.guessedWords.length === 0) {
+    content = (
+      <span data-test="guess-instructions">Try to guess the secret word!</span>
+    );
+  }
+  return <div>{content}</div>;
 };
 
-GuessedWords.propTypes = {};
+GuessedWords.propTypes = {
+  guessedWords: PropTypes.arrayOf(
+    PropTypes.shape({
+      guessedWord: PropTypes.string.isRequired,
+      letterMatchCount: PropTypes.number.isRequired
+    })
+  ).isRequired
+};
 
 export default GuessedWords;
--- src/components/GuessedWords.test.js
+++ src/components/GuessedWords.test.js
@@ -1,2 +1,41 @@
 import { findByTestAttribute, checkPropsError } from '../test/test-utils';
 import GuessedWords from './GuessedWords';
+
+const defaultProps = {
+  guessedWords: [
+    {
+      guessedWord: 'train',
+      letterMatchCount: 3
+    }
+  ]
+};
+
+const setup = (props = {}) => {
+  const setupProps = {
+    ...defaultProps,
+    ...props
+  };
+
+  const wrapper = shallow(<GuessedWords {...setupProps} />);
+  return wrapper;
+};
+
+it('does not throw warning with expected props', () => {
+  checkPropsError(GuessedWords, defaultProps);
+});
+
+describe('if there are no words guessed', () => {
+  // we can run instructions before each test and provide
+  // wrapper to all tests
+  let wrapper;
+  beforeEach(() => {
+    wrapper = setup({ guessedWords: [] });
+  });
+
+  test('renders instructions to guess a word', () => {
+    const instructions = findByTestAttribute(wrapper, 'guess-instructions');
+    expect(instructions.text().length).not.toBe(0);
+  });
+});
+
+describe('if there are words guessed', () => {});
