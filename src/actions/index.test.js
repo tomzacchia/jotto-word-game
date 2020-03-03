ae8c71e1b6a4290c7220da8b739cc86c14ed08db
tomzacchia <tomzacchia@live.com>
2020-03-02 19:04:44 -0500

    Add correctGuess action

---
+++ src/actions/index.test.js
@@ -0,0 +1,18 @@
+import { correctGuess, actionTypes } from './';
+
+describe('correctGuess', () => {
+  test('returns an action with type `CORRECT_GUESS`', () => {
+    const action = correctGuess();
+    const expectedAction = {
+      type: actionTypes.CORRECT_GUESS
+    };
+    // deep equal
+    expect(action).toEqual(expectedAction);
+    /*
+      toBe cannot be used with mutable data types sice their contents
+      are technically not the same despite their apperance,
+      i.e {} !== {}
+    */
+    // expect(action).toBe({});
+  });
+});
