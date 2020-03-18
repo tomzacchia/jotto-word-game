d3ddc460de2c3ea86cc57ae776d78fc6ff0cf165
tomzacchia <tomzacchia@live.com>
2020-03-17 21:08:42 -0400

    Remove correctGuess action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,9 @@
 export const actionTypes = {
-  CORRECT_GUESS: 'CORRECT_GUESS'
+  CORRECT_GUESS: 'CORRECT_GUESS',
+  GUESS_WORD: 'GUESS_WORD'
 };
 
-export function correctGuess() {
-  return { type: actionTypes.CORRECT_GUESS };
-}
+export const guessWord = guessedWord => {
+  // Thunk middleware provides these two arguments to returned function
+  return function(dispatch, getState) {};
+};
